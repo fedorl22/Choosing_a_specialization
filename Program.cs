51902538feae6248b51b8e56b478eb87c1caf3a1
Fedor <fdr@mail.ru>
2022-12-25 23:52:02 +0300

    add_program_description

--- Program.cs
+++ Program.cs
@@ -4,3 +4,19 @@
 //При решении не рекомендуется пользоваться
 // коллекциями, лучше обойтись исключительно массивами.
 
+string[] A;
+Console.WriteLine("Введите массив строк через запятую");
+string stroka = Console.ReadLine();
+A = stroka.Split(',');
+var result = new string[A.Length];
+var realSize = 0;
+foreach (var value in A)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
